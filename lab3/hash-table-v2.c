4329d490cad9b073114daf766da8a30c5d26aede
ryanchaiyakul <ryanchaiyakul@gmail.com>
2024-05-23 22:15:22 -0700

    ?

--- lab3/hash-table-v2.c
+++ lab3/hash-table-v2.c
@@ -86,16 +86,15 @@ void hash_table_v2_add_entry(struct hash_table_v2 *hash_table,
 
 	/* Update the value if it already exists */
 	if (list_entry != NULL) {
-		pthread_mutex_unlock(&hash_table_entry->mutex);	// -5 points otherwise
 		list_entry->value = value;
+		pthread_mutex_unlock(&hash_table_entry->mutex);	// -5 points otherwise
 		return;
 	}
 	list_entry = calloc(1, sizeof(struct list_entry));
-	SLIST_INSERT_HEAD(list_head, list_entry, pointers);
-	pthread_mutex_unlock(&hash_table_entry->mutex);
 	list_entry->key = key;
 	list_entry->value = value;
-	
+	pthread_mutex_unlock(&hash_table_entry->mutex);
+	SLIST_INSERT_HEAD(list_head, list_entry, pointers);
 }
 
 uint32_t hash_table_v2_get_value(struct hash_table_v2 *hash_table,
