479d3eb4eff6eb13ab20f4e3e218997b7cfd6b32
ryanchaiyakul <ryanchaiyakul@gmail.com>
2024-05-24 17:56:48 -0700

    trying new locking scheme

--- lab3/hash-table-v2.c
+++ lab3/hash-table-v2.c
@@ -83,22 +83,31 @@ void hash_table_v2_add_entry(struct hash_table_v2 *hash_table,
 	if (error) { 
 		exit(error);
 	}
-	struct list_head *list_head = &hash_table_entry->list_head;	// can leave this outside as the address will not change
-	
+	struct list_head *list_head = &hash_table_entry->list_head;
 	struct list_entry *list_entry = get_list_entry(hash_table, key, list_head);
-
 	/* Update the value if it already exists */
 	if (list_entry != NULL) {
 		list_entry->value = value;
 		int error = pthread_mutex_unlock(&hash_table_entry->mutex);	// -5 points otherwise
 		if (error) { 
-		exit(error);
-	}
+			exit(error);
+		}
 		return;
 	}
+	error = pthread_mutex_unlock(&hash_table_entry->mutex);
+	if (error) { 
+		exit(error);
+	}
+
 	list_entry = calloc(1, sizeof(struct list_entry));
 	list_entry->key = key;
 	list_entry->value = value;
+	
+	int error = pthread_mutex_lock(&hash_table_entry->mutex);
+	if (error) { 
+		exit(error);
+	}
+	list_head = &hash_table_entry->list_head;
 	SLIST_INSERT_HEAD(list_head, list_entry, pointers);
 	error = pthread_mutex_unlock(&hash_table_entry->mutex);
 	if (error) { 
